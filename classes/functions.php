5e64ca40bcfb9e28c0dae47e77e4bb19c4d133a0
lishun <401068081@qq.com>
2015-08-04 15:51:32 +0800

    整理：将系统用的函数放在 core.php, 其他通用函数放在 functions.php

--- classes/functions.php
+++ classes/functions.php
@@ -1,38 +1,42 @@
 <?php
 
-function error($msg="file_not_found"){
-	echo "error: $msg";
-	return false;
-}
-
-function loadClasses($className){
-	$path=APP_PATH."/";
-
-	if (preg_match("/^[A-Z]\w*Model$/", $className)){
-		$path = APP_PATH."/".M."/models/";
-	}elseif (preg_match("/[A-Z]\w*Model$/", $className)){
-		$parts = explode("\\", $className);
-		$path = APP_PATH."/".$parts[0]."/models/";
-		list(, $className) = explode("\\", $className);
-		echo "classname=$className, path=$path<br>\r\n";
-	}else{
-		$path = APP_PATH."/classes/";
-	}
-	//echo "classname=$className, path=$path<br>\r\n";
-    set_include_path($path);
-    spl_autoload($className);
-}
-
-function newClass($className, $module=''){
-	$className = strtolower(str_ireplace("model", "", $className));
-	if ($module=="") $module = M;
-	require $module."/models/".$className."Model.php";
-	$modelName = ucfirst($className)."Model";
-	return new $modelName();
-}
-
-
 function element($key, $arr = array() , $default_value = "") {
     if (is_array($arr) && isset($arr[$key])) return $arr[$key];
     return $default_value;
+}
+
+
+// 不区分大小写的in_array实现
+function in_array_case($value,$array){
+    return in_array(strtolower($value),array_map('strtolower',$array));
+}
+
+/**
+ * 获取客户端IP地址
+ * @param integer $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
+ * @param boolean $adv 是否进行高级模式获取（有可能被伪装）
+ * @return mixed
+ */
+function get_client_ip($type = 0,$adv=false) {
+    $type       =  $type ? 1 : 0;
+    static $ip  =   NULL;
+    if ($ip !== NULL) return $ip[$type];
+    if($adv){
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            $pos    =   array_search('unknown',$arr);
+            if(false !== $pos) unset($arr[$pos]);
+            $ip     =   trim($arr[0]);
+        }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip     =   $_SERVER['REMOTE_ADDR'];
+        }
+    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ip     =   $_SERVER['REMOTE_ADDR'];
+    }
+    // IP地址合法验证
+    $long = sprintf("%u",ip2long($ip));
+    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+    return $ip[$type];
 }
--- index.php
+++ index.php
@@ -21,12 +21,22 @@ define('IS_CLI',PHP_SAPI=='cli'? 1   :   0);
 
 
 require("classes/functions.php");
+require("classes/core.php");
+
+
+/**
+ * 命令行的调用方式:
+ *  php index.php "m=home&c=game&a=list&key1=$value1&key2=$value2&key3=$value3&key4=$value4"
+ *  其中 m,c,a 三个参数是必须的，指明调用哪个模块(M)的哪个控制器(C)的哪个动作/方法(A)
+ *  程序会把所有参数转化成 $_GET数组。
+*/
 if (IS_CLI && intval($_SERVER["argc"])>1) parse_str($_SERVER["argv"][1], $_GET);
 
 !empty(element('m', $_GET)) && define("M", strtolower(element('m', $_GET)));
 !empty(element('c', $_GET)) && define("C", strtolower(element('c', $_GET)));
 !empty(element('a', $_GET)) && define("A", strtolower(element('a', $_GET)));
 
+// 浏览器访问，有开 url_rewrite
 $uri = element("PHP_SELF", $_SERVER);
 if (strpos($uri, "index.php/")!==false){
 	$parts= explode("/", strstr($uri, "index.php/"));
@@ -37,16 +47,17 @@ if (strpos($uri, "index.php/")!==false){
 	}
 }
 
+// 默认情况访问默认的控制器
 defined('M') or define("M", 'home');
 defined('C') or define("C", 'home');
 defined('A') or define("A", 'index');
 
+// 保证这些参数是干净的
 (preg_match("/^\w+$/", M) && preg_match("/^\w+$/", C) && preg_match("/^\w+$/", A)) or die(error());
 
 //echo "/* ".M."::".C."::".A." */";
-//
-//
-//
+
+
 spl_autoload_extensions('.php');
 spl_autoload_register('loadClasses');
 
